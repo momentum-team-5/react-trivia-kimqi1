759a9d5c17f4ba2da6db37333715ffa33e79e511
Kim Leibowitz <kim@brightlightwellness.com>
2020-11-17 16:44:00 -0500

    Attempted import error: './components/TriviaCategory' does not contain a default export (imported as 'TriviaCategory').

--- src/App.js
+++ src/App.js
@@ -2,21 +2,44 @@
 import 'tachyons'
 import './App.css'
 import { useEffect, useState } from 'react'
+import TriviaCategory from './components/TriviaCategory'
 
 function App () {
   const [triviaCat, setTriviaCat] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState(null)
 
   useEffect(() => {
     fetch('https://opentdb.com/api_category.php')
       .then(response => response.json())
-      .then(data => setTriviaCat(data.trivia_categories))
+      .then(data => {
+        return setTriviaCat(data.trivia_categories)
+      })
   }, [])
 
+  if (selectedCategory) {
+    return (
+      <TriviaCategory
+        category={selectedCategory}
+        clearSelectedCategory={() => setSelectedCategory(null)}
+      />
+    )
+  }
   return (
     <div className='mw7 center pa2'>
+      <h2>Trivia Categories</h2>
       <ul>
         {triviaCat.map((category) => (
-          <li key={category.id}>{category.name}</li>
+          <li key={category.id}>{category.name}
+            <div className='f4 w5'>
+              <button
+                onClick={() => setTriviaCat(category)}
+                className='pl0 bw0 bg-white underline pointer blue'
+              >
+                {category.Category}
+              </button>
+            </div>
+
+          </li>
         ))}
       </ul>
     </div>
