97d7c7e068efec1a1cc7ce46265008a13b0fb9ea
Kim Leibowitz <kim@brightlightwellness.com>
2020-11-22 12:13:46 -0500

    shuffled answers

--- src/components/TriviaQuestions.js
+++ src/components/TriviaQuestions.js
@@ -4,15 +4,29 @@ import 'tachyons'
 export default function TriviaQuestions (props) {
   const { category, clearSelectedCategory } = props
   const [questions, setQuestions] = useState([])
-  const [answer, setAnswer] = ('')
+  // const [answers, setAnswers] = useState(null)
 
   useEffect(() => {
     fetch('https://opentdb.com/api.php?amount=10' + '&category=' + category.id)
       .then(res => res.json())
       .then(data => {
+        for (const question of data.results) {
+          const answers = question.incorrect_answers.slice()
+          answers.push(question.correct_answer)
+          // shuffle answers
+          question.shuffled_answers = answers
+        }
+
         setQuestions(data.results)
       })
   }, [category])
+
+  /* useEffect(() => {
+    const answers = incorrect_answers.slice()
+    TriviaQuestion.incorrect_answers.slice()
+    answers.push(answers.correct_answer)
+  }, [TriviaQuestion])
+*/
 
   return (
     <div>
@@ -24,7 +38,7 @@ export default function TriviaQuestions (props) {
             <div className='bg blue'>Question: <span dangerouslySetInnerHTML={{ __html: name.question }} /></div>
             <div className='bg green'>Correct Answers: <span dangerouslySetInnerHTML={{ __html: name.correct_answer }} /></div>
             <div className='bg red'>Incorrect Answers: <span dangerouslySetInnerHTML={{ __html: name.incorrect_answers }} /></div>
-
+            <div className='bg red'>Shuffled Answers Answers: <span dangerouslySetInnerHTML={{ __html: name.shuffled_answers.join(', ') }} /></div>
           </li>
         ))}
       </ul>
